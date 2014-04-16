0372016e26f9ad0e6063ae1fa1b45818755603c8
Daniel Lissner <dlissner@gmail.com>
2014-04-16 14:42:29 -0500

    Implement trees estimator

--- app.rb
+++ app.rb
@@ -1,11 +1,12 @@
 require 'sinatra/base'
+require 'date'
 
 class Snaas < Sinatra::Application
   get '/names' do
     'list of names'
   end
 
-  get 'names/:name' do
+  get '/names/:name' do
     'details of #{params[:name]} '
   end
 
@@ -13,15 +14,22 @@ class Snaas < Sinatra::Application
     'list of albums'
   end
 
-  get 'albums/:album_name' do
+  get '/albums/:album_name' do
     'details of #{params[:album_name]}'
   end
 
-  get 'trees' do
-    'est. amount of weed snoop has smoked so far'
+  get '/trees' do
+    blunts_in_life = 81 * 365 * (Date.today.mjd - Date.today.mjd - Date.new(1971,10,20).mjd)
+
+    {
+      :blunts_per_day       => 81,
+      :blunts_in_life       => blunts_in_life,
+      :est_grams_per_blunt  => 1.4,
+      :total_grams          => 1.4 * blunts_in_life
+    }
   end
 
-  get 'doggpound' do
+  get '/doggpound' do
     'random photo of snoop'
   end
 end
