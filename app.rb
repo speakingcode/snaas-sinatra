7bacc4547c4cc54b97b15f56246980d29f8fcb0d
Daniel Lissner <dlissner@gmail.com>
2014-04-16 14:50:03 -0500

    Fix tree estimation

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'json'
 require 'date'
 
 class Snaas < Sinatra::Application
@@ -19,14 +20,15 @@ class Snaas < Sinatra::Application
   end
 
   get '/trees' do
-    blunts_in_life = 81 * 365 * (Date.today.mjd - Date.today.mjd - Date.new(1971,10,20).mjd)
+    #birthdate was 1971, assume started smoking at 15
+    blunts_in_life = 81 * (Date.today.mjd - Date.new(1986,10,20).mjd)
 
     {
       :blunts_per_day       => 81,
       :blunts_in_life       => blunts_in_life,
       :est_grams_per_blunt  => 1.4,
       :total_grams          => 1.4 * blunts_in_life
-    }
+    }.to_json
   end
 
   get '/doggpound' do
