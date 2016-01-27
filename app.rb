543462b04ef8b6bc88331dbf0e81a194871e3427
speakingcode <dlissner@gmail.com>
2016-01-27 01:32:43 -0600

    Show individual albums

--- app.rb
+++ app.rb
@@ -59,8 +59,39 @@ class Snaas < Sinatra::Application
     albums.map { |album| {:id => album[:id], :name => album[:name]}}.to_json
   end
 
-  get '/albums/:album_name' do
-    'details of #{params[:album_name]}'
+  get '/albums/:id' do
+
+   albums = [
+      {
+        :id           => 1,
+        :name         => 'Doggystyle',
+        :release_date => 'November 23, 1993',
+        :label        => 'Death Row',
+        :length       => '53:24',
+        :img_url      => 'https://upload.wikimedia.org/wikipedia/en/6/63/SnoopDoggyDoggDoggystyle.jpg'
+      },
+
+      {
+        :id           => 2,
+        :name         => 'The Doggfather',
+        :release_date => 'November 12, 1996',
+        :label        => 'Death Row',
+        :length       => '74:13',
+        :img_url      => 'https://upload.wikimedia.org/wikipedia/en/a/a3/Tha-doggfather.jpg'
+      },
+
+      {
+        :id           => 3,
+        :name         => 'Da Game Is to Be Sold, Not to Be Told',
+        :release_date => 'August 14, 1998',
+        :label        => 'No Limit',
+        :length       => '76:03',
+        :img_url      => 'https://upload.wikimedia.org/wikipedia/en/c/c5/Gameistobesold.jpg'
+      }
+
+    ]
+   puts params['id'].class
+   albums.select{ |album| album[:id] == params['id'].to_i}.first.to_json
   end
 
   get '/trees' do
